53bf6c253bbba2638668f197daccb910e68bf03e
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-07-04 06:04:36 -0400

    react dom

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Professional from '../../assets/images/Professional.jpg'
 
 function Home() {
