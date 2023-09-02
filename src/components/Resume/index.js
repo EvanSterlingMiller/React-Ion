d556a7ac369cc3127431711589c6d5e780079dd7
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-09-02 19:25:12 -0400

    fixes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Resume() {
     return(
         <section>
-            <h1 href='../../assets/Resume.pdf' download>View my resume.</h1>
+            <h1 href='../../assets/Resume.pdf' download = "Resume">View my resume.</h1>
         </section>
     )
 }
